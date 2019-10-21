1c914a1ad0a5762941b860d1c5d8fb4546690f8e
Diego Guedes <diegoamericoguedes@gmail.com>
2019-10-21 07:58:51 -0300

    Update Main.java

--- rmi_com_persistencia/Main.java
+++ rmi_com_persistencia/Main.java
@@ -6,15 +6,11 @@ public class Main {
 	public static void main(String[] args) {
 		ContaDAOImplementacao contaDAO = new ContaDAOImplementacao();
 
-		Conta contaJoao = new Conta();
-		contaJoao.setId(6);
-		contaJoao.setNome("José");
-		contaJoao.setSaldo(new BigDecimal(100));
-
-		contaDAO.inserir(contaJoao);
-
 		Conta contaJoaoConsulta = contaDAO.consultar("José");
 		System.out.println(contaJoaoConsulta.toString());
+
+		Conta contaId5 = contaDAO.consultar(5);
+		contaDAO.excluir(contaId5);
 
 	}
 }
